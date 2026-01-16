d0273f60f9ee5095d7b9e41cbb6a6ce7ef499768
andre <vebriandreadi15@gmail.com>
2026-01-17 01:05:31 +0700

    Update Full

--- config.py
+++ config.py
@@ -4,6 +4,16 @@ import datetime,pytz
 
 # load_dotenv()
 
+# ==== SETTINGS 
+SETTINGS_SCHEMA = {
+    "debug": "bool",
+    "start_info_state": "bool",
+    "start_info": "text",
+    "vip_info_state": "bool",
+    "vip_info": "text",
+    "group": "text",
+}
+
 # ===== BASE =====
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
