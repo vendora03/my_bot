2232a841e6408f5ed9eaacbf205dc2870c097720
andre <vebriandreadi15@gmail.com>
2026-08-16 15:00:12 +0700

    Update Auto

--- services/maintenance.py
+++ services/maintenance.py
@@ -13,7 +13,7 @@ def maintenance(state):
             raw_durasi = int(time.time()) - maintenance_start
             durasi = str(timedelta(seconds=raw_durasi))
             print(f"==============================\n[INFO] BOT ALREADY IN MAINTENANCE\nDuration: {durasi}\n==============================")    
-            Send_Message_Admin(f"<b>[INFO] BOT ALREADY IN MAINTENANCE</b>\nDuration: {durasi}", None)
+            Send_Message_Admin(f"<i><b>[INFO] BOT ALREADY IN MAINTENANCE</b></i>\nDuration: {durasi}", None)
             return
         
         settings.set("maintenance", state)
@@ -21,11 +21,11 @@ def maintenance(state):
         waktu_mulai = datetime.fromtimestamp(int(maintenance_start))
         settings.set("maintenance_start", maintenance_start)
         print(f"==============================\n[INFO] BOT IN MAINTENANCE MODE\nTime: {waktu_mulai.strftime('%d/%m/%Y %H:%M:%S')}\n==============================")
-        Send_Message_Admin(f"<b>[INFO] BOT IN MAINTENANCE MODE</b>\nTime: {waktu_mulai.strftime('%d/%m/%Y %H:%M:%S')}", None)
+        Send_Message_Admin(f"<i><b>[INFO] BOT IN MAINTENANCE MODE</b></i>\nTime: {waktu_mulai.strftime('%d/%m/%Y %H:%M:%S')}", None)
     else:
         if settings.get("maintenance") == state:
             print(f"==========================\n[INFO] BOT IN WORKING MODE\n==========================")
-            Send_Message_Admin(f"<b>[INFO] BOT IN WORKING MODE</b>", None)
+            Send_Message_Admin(f"<i><b>[INFO] BOT IN WORKING MODE</b></i>", None)
             return
         
         maintenance_start = int(settings.get("maintenance_start"))
@@ -34,7 +34,7 @@ def maintenance(state):
         print(f"==========================\n[INFO] BOT IN WORKING MODE\nDuration: {durasi}\n==========================")
         settings.set("maintenance", state)
         settings.set("maintenance_start", "")
-        Send_Message_Admin(f"<b>[INFO] BOT IN WORKING MODE</b>\nDuration: {durasi}", None)
+        Send_Message_Admin(f"<i><b>[INFO] BOT IN WORKING MODE</b></i>\nDuration: {durasi}", None)
 
 if __name__ == "__main__":
     task = sys.argv[1].lower() if len(sys.argv) > 1 else None
